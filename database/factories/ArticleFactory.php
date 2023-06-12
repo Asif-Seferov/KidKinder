2f3eff120f19e771433ec84b7c2f0749d45ce15b
Asif Seferov <asif_seferov_1995@mail.ru>
2023-06-12 08:15:27 +0400

    insert fake data to Article model

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
@@ -16,8 +17,21 @@ class ArticleFactory extends Factory
      */
     public function definition()
     {
+        $name = $this->faker->name;
         return [
-            //
+            'name' => $name,
+            'category_id' => random_int(1,10),
+            'user_id' => random_int(1, 10),
+            'slug' => Str::slug($name),
+            'body' => $this->faker->text,
+            'status' => $this->faker->boolean,
+            'tags' => random_int(1,10),
+            'seo_keywords' => Str::slug($this->faker->address, ", "),
+            'seo_description' => $this->faker->text,
+            'read_count' => random_int(0, 2000),
+            'like_count' => random_int(1, 100),
+            'view_count' => random_int(1, 100),
+            'publish_date' => $this->faker->dateTime
         ];
     }
 }
--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Article;
 
 class ArticleSeeder extends Seeder
 {
@@ -14,6 +15,6 @@ class ArticleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Article::factory(10)->create();
     }
 }
